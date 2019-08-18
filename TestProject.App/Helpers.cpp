ce94733c6cd86a36e88b07bed884279667e93f8d
Victor <victor.redler@collectorbank.se>
2019-08-18 22:22:02 +0200

    HelperMethod namespace

---
+++ TestProject.App/Helpers.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+namespace ConsoleHelper
+{
+	void GetKey()
+	{
+		std::cout << "Press any key to continue..." << std::endl;
+
+		std::cin.get();
+	}
+}
---
+++ TestProject.App/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "Helpers.h"
+
+int main()
+{
+	std::cout << "Hello World" << std::endl;
+
+	ConsoleHelper::GetKey();
+	return 0;
+}
